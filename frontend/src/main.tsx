8d512a64ad85dbd1ee6622bf4e1a7714262deeb9
Kai <3022677@stud.th-mannheim.de>
2026-04-26 20:34:49 +0200

    Kleiner Bugfix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { PrimeReactProvider } from 'primereact/api'
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
 import './index.css'
-import App from './App'
+import App from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
